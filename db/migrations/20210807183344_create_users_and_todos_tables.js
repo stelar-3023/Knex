d1f2a8c192054a161364024c4704a56fa27195dd
Steve Larsen <slarsen-3@att.net>
2021-08-07 18:56:20 -0500

    migrations

---
+++ db/migrations/20210807183344_create_users_and_todos_tables.js
@@ -0,0 +1,22 @@
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .createTable("users", function (table) {
+      table.increments();
+      table.string("name").notNullable();
+      table.string("email").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+    .createTable("todos", function (table) {
+      table.increments();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+      table.string("title").notNullable();
+      table.boolean("completed").defaultTo(false);
+      table.integer("user_id").references("id").inTable("users");
+    });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable("todos").dropTable("users");
+};
