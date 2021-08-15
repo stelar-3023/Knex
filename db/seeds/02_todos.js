0204ac97da432fe2a9d90a9e8b7ef5389b93cb39
Steve Larsen <slarsen-3@att.net>
2021-08-15 12:57:07 -0500

    updated seeds with data

--- db/seeds/02_todos.js
+++ db/seeds/02_todos.js
@@ -1,16 +1,16 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex("todos")
     .del()
     .then(function () {
       // Inserts seed entries
       return knex("todos").insert([
-        { id: 1, title: "go to store for milk", user_id: 1 },
-        { id: 2, title: "walk the dog", user_id: 2 },
-        { id: 3, title: "go to the gym", user_id: 3 },
-        { id: 4, title: "stop the damn leafblowers outside", user_id: 3 },
-        { id: 5, title: "get the mail", user_id: 3 },
-        { id: 6, title: "get some headphones", user_id: 3 },
+        { title: "go to store for milk", user_id: 1 },
+        { title: "walk the dog", user_id: 2 },
+        { title: "go to the gym", user_id: 3 },
+        { title: "mow the yard", user_id: 3 },
+        { title: "get the mail", user_id: 3 },
+        { title: "get some headphones", user_id: 3 },
       ]);
     });
 };
