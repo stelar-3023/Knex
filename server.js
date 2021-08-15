5d93400621a481b4ff871a271b90f23cccef33a2
Steve Larsen <slarsen-3@att.net>
2021-08-15 12:59:22 -0500

    added all routes for todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var port = process.env.PORT || 8001;
+var cors = require("cors");
 var knex = require("./db/knex");
 
 var app = express();
@@ -8,25 +9,99 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get("/todos", function (req, res) {
-//   knex.raw("SELECT * FROM todos").then(function (todos) {
-//     res.send(todos.rows);
-//   });
-// });
+// Routes
+// Get all todos
+app.get("/todos", function (req, res) {
+  knex
+    .select()
+    .from("todos")
+    .then(function (todos) {
+      res.send(todos);
+    });
+});
+
+app.get("/todos-of-user/:id", function (req, res) {
+  knex
+    .from("todos")
+    .innerJoin("users", "todos.user_id", "users.id")
+    .where("todos.user_id", req.params.id)
+    // .where(knex.raw("users.id = ?", [req.params.id]))
+    .then(function (data) {
+      res.send(data);
+    }).catch(function (err) {
+      console.log(err);
+    })
+});
 
 // app.get("/todos", function (req, res) {
 //   knex.raw("SELECT * FROM todos where id = 1").then(function (todo) {
 //     res.send(todo.rows);
 //   });
 // });
-
-app.get("/todos", function (req, res) {
+// Get a single todo
+app.get("/todos/:id", function (req, res) {
   knex
     .select()
     .from("todos")
-    .where("id", 1)
-    .then(function (todo) {
-      res.send(todo);
+    .where("id", req.params.id)
+    .then(function (todos) {
+      res.send(todos);
+    })
+    .catch(function (err) {
+      res.send(err);
+    });
+});
+
+// Insert a todo
+app.post("/todos", function (req, res) {
+  knex("todos")
+    .insert({
+      title: req.body.title,
+      user_id: req.body.user_id,
+    })
+    .then(function () {
+      knex
+        .select()
+        .from("todos")
+        .then(function (todos) {
+          res.send(todos);
+        });
+    });
+});
+
+// Update a todo
+app.put("/todos/:id", function (req, res) {
+  knex("todos")
+    .where("id", req.params.id)
+    .update({
+      title: req.body.title,
+      completed: req.body.completed,
+    })
+    .then(function () {
+      knex
+        .select()
+        .from("todos")
+        .then(function (todos) {
+          res.send(todos);
+        });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+//Delete a todo
+app.delete("/todos/:id", function (req, res) {
+  knex("todos")
+    .where("id", req.params.id)
+    .del()
+    .then(function () {
+      knex
+        .select()
+        .from("todos")
+        .then(function (todos) {
+          res.send(todos);
+        });
     });
 });
 
