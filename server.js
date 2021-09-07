d604bccaed97e9b31851a9270f70ea8ff8779665
Steve Larsen <slarsen-3@att.net>
2021-09-07 17:09:18 -0500

    initial commit

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/", function(req, res) {
+  res.send("Home Page");
+  });
 
 // Routes
 // Get all todos
